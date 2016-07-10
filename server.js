cc7e7e58175ae81e2789c072fbb609d90a81f649
Tanner Coval <tannercoval@gmail.com>
2016-07-09 20:55:03 -0700

    modified package.json for deployment configurations and fixed server.js port configuration

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ var express = require('express'),
 path = require('path'),
 app = express();
 
-app.use(express.static('public'));
+app.set('port', (process.env.PORT || 8000));
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.listen(8000);
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
+});
